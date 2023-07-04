c3977ae1f720449e7d38a02ef06595b04fc1bc41
Pratik <shringarpure95@gmail.com>
2023-07-04 21:08:33 +0530

    final commit

--- script.js
+++ script.js
@@ -1,80 +1,72 @@
-const bookURL = 'https://anapioficeandfire.com/api';
-async function fetchbooks() {
-    try{
-    const response  = await fetch(`${bookURL}/books`);
-    if(!response.ok){
-        throw new Error(`Failed to fetch books: ${response.status}`);
-    }
-    return await response.json();
+let informationDiv = document.querySelector('.informationDiv');
+let bookDetailsDiv = document.querySelector('.bookDetailsDiv');
+let bookListlink = document.querySelector('.bookListlink');
+
+function getImageLink(bookName){
+    let imageLink;
     
-    }catch(e) {
-        console.log(e);
+    return imageLink;
+}
+
+function loadCardDetails(dataObject, imageLink){
+    let card = `
+        <div class="card border-light" style="max-width: 540px;">
+             <div class="row g-0">
+                  <div class="col-md-4">
+                       <img src=${imageLink} class="card-img-top" alt=${dataObject.name} height="250px"; width="150px">
+                  </div>
+                  <div class="col-md-8">
+                       <div class="card-body">
+                            <h5 class="card-title">${dataObject.name}</h5>
+                            <p class="card-text">Author:${dataObject.authors[0]}</p>
+                            <p class="card-text">ISBN:${dataObject.isbn}</p>
+                            <p class="card-text">Number of Pages:${dataObject.numberOfPages}</p>
+                            <p class="card-text">Publisher:${dataObject.publisher}</p>
+                            <p class="card-text">Released at:${dataObject.released}</p>
+                       </div>
+                  <div>
+             </div>
+        </div>`;
+    bookDetailsDiv.innerHTML = card;
+}
+
+function loadBookDetails(bookObjectList, bookName){
+    for(let dataObject of bookObjectList){
+        if(dataObject.name === bookName){
+            let imageLink = getImageLink(bookName);
+            loadCardDetails(dataObject, imageLink);
+        }
     }
 }
 
-function listsPosts(postContainerElementId){
-    const postContainerElement = document.getElementById(postContainerElementId);
-
-    if(!postContainerElement){
-        return;
-    }
-    fetchbooks()
-    .then((books ) =>{
-        if(!books){
-            postContainerElement.innerText = "No Books";
-                return;
-        }
-
-        for(const Books of books){
-            postContainerElement.appendChild(postElement(books));
-        }
-    })
-    .catch(e => {
-        console.log(e);
-    })
-}
-function postElement(books){
-    const anchorElement = document.createElement('a');
-    anchorElement.setAttribute('href',`${bookURL}/books/${books.isbn}`);
-    anchorElement.setAttribute('target','_blank');
-    anchorElement.innerText = books.postTitleElement;
-
-    const postTitleElement = document.createElement('h4');
-    postTitleElement.appendChild(anchorElement);
-
-    return postTitleElement;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const books = await response.json();
-
-    return books;
-
-document.addEventListener("DOMContentLoaded", async() =>{
-    let books = [];
+async function loadData(){
 
     try{
-        books = await loadBooks();
-    }catch(e){
-        console.log("Error!");
-        console.log(e);
+        let response = await fetch(`https://anapioficeandfire.com/api/books/`);
+        let bookObjectList = await response.json();
+        let list = document.createElement('ul');
+        for(let bookObject of bookObjectList){
+            let listItems = document.createElement('li');
+            let labelLink = document.createElement('label');
+            labelLink.classList.add('informationLabel');
+            labelLink.innerHTML = `${bookObject.name}`
+            labelLink.style.cursor = 'pointer';
+            listItems.append(labelLink);
+            list.append(listItems);
+        }
+        informationDiv.innerHTML = '';
+        informationDiv.append(list);
+        let labels = document.querySelectorAll('.informationLabel');
+        labels.forEach(label => label.addEventListener('click', e =>{
+            let bookName = e.currentTarget.innerHTML;
+            loadBookDetails(bookObjectList, bookName);
+        }));
+    }catch(error){
+        console.log('error loading the book name!!!!!')
     }
-    console.log(books);
+}
+
+bookListlink.addEventListener('click', function(){
+    loadData();
 });
 
-/*fetch("https://www.anapioficeandfire.com/api/books/1")
-.then(res => res.json())
-.then(data => console.log(data));
-*/
